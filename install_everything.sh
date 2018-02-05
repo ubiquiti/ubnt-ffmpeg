beaf74f2f502f028a5c167271c3c231cdc5dec83
Gavriloaie Eugen-Andrei <andrei@ubnt.com>
2018-02-05 23:39:45 +0200

    -- fixed the install script

--- install_everything.sh
+++ install_everything.sh
@@ -3,35 +3,15 @@
 
 set -e
 
-# if [ ! -f ${LOCAL_PREFIX}/lib/libfaac.a ]
-# then
-# 	FAAC_VERSION=1.28
-# 	FAAC_BASE_NAME=faac-${FAAC_VERSION}
-# 	FAAC_ZIP_NAME=${FAAC_BASE_NAME}.zip
-# 	FAAC_URL=http://freefr.dl.sourceforge.net/project/faac/faac-src/${FAAC_BASE_NAME}/${FAAC_ZIP_NAME}
-
-# 	(cd /tmp \
-# 		&& curl ${FAAC_URL} -o ${FAAC_ZIP_NAME} \
-# 		&& tar xvf ${FAAC_ZIP_NAME} \
-# 		&& cd ${FAAC_BASE_NAME} \
-# 		&& for i in `find . -type f`;do dos2unix $i;done \
-# 		&& ./bootstrap \
-# 		&& ./configure --disable-shared --enable-static --without-mp4v2 --prefix=${LOCAL_PREFIX} \
-# 		&& make -j32 install \
-# 	)
-# fi
+DEST_FOLDER=${LOCAL_PREFIX}/ffmpeg_install
 
 ./configure \
-	--prefix=${LOCAL_PREFIX} \
-	--extra-cflags="-I${LOCAL_PREFIX}/include" \
-	--extra-ldflags="-L${LOCAL_PREFIX}/lib" \
+	--prefix=${DEST_FOLDER} \
+	--extra-cflags="-I${LOCAL_PREFIX}/include -I${DEST_FOLDER}/include" \
+	--extra-ldflags="-L${LOCAL_PREFIX}/lib -L${DEST_FOLDER}/lib" \
 	--enable-gpl \
 	--enable-libx264 \
 
-
-rm -rf ${LOCAL_PREFIX}/include/libav*
-rm -rf ${LOCAL_PREFIX}/lib/libav*
-rm -rf ${LOCAL_PREFIX}/lib/pkgconfig/libav*
 
 make -j32 install
 
