bb3a9c67675f95a2110d7469fde1059047e797af
Gavriloaie Eugen-Andrei <andrei@ubnt.com>
2018-06-10 04:27:54 +0300

    -- trimmed down the necessary features

--- debug.sh
+++ debug.sh
@@ -1,21 +1,26 @@
-#/bin/sh
+#!/bin/sh
 
-set -e
+set -ex
 
-export TEMP_PREFIX=/Users/shiretu/work/evostream/builders/cmake/external_libs/libav
-export LIBAV_EXTRA_CFLAGS='-mmacosx-version-min=10.11'
+export TEMP_PREFIX=/tmp/fftests/external_libs/ffmpeg
+export FFMPEG_EXTRA_CFLAGS='-mmacosx-version-min=10.11'
+
+git clean -dxff ./
 
 if [ ! -f ./.configured ]
 then
 	./configure \
 		--prefix=${TEMP_PREFIX} \
-		--extra-cflags="${LIBAV_EXTRA_CFLAGS} -I${TEMP_PREFIX}/include -g" \
+		--extra-cflags="${FFMPEG_EXTRA_CFLAGS} -I${TEMP_PREFIX}/include -g" \
 		--extra-ldflags="-L${TEMP_PREFIX}/lib" \
 		--enable-debug \
 		--disable-optimizations \
-		`cat ./features.txt|tr "\n" " "`
+		`cat ./features.txt|grep -v "^#"|tr "\n" " "`
 	>./.configured
 fi
 
+mv ${TEMP_PREFIX}/lib/libx264.a /tmp
+rm -rf ${TEMP_PREFIX}
 make -j32 install
-
+mv /tmp/libx264.a ${TEMP_PREFIX}/lib/
+>${TEMP_PREFIX}/.extracted
