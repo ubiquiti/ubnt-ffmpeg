5461613f908998c0033fda5f037dd29a870a4aa9
Gavriloaie Eugen-Andrei <andrei@ubnt.com>
2019-12-04 00:53:22 +0200

    -- minor

--- install_everything.sh
+++ install_everything.sh
@@ -7,12 +7,11 @@ DEST_FOLDER=${LOCAL_PREFIX}/ffmpeg_install
 
 ./configure \
 	--prefix=${DEST_FOLDER} \
-	--extra-cflags="-I${LOCAL_PREFIX}/include -I${DEST_FOLDER}/include" \
+	--extra-cflags="-fno-stack-check -I${LOCAL_PREFIX}/include -I${DEST_FOLDER}/include" \
 	--extra-ldflags="-L${LOCAL_PREFIX}/lib -L${DEST_FOLDER}/lib" \
 	--enable-gpl \
 	--enable-nonfree \
-	--enable-libx264 \
-	--enable-libfdk-aac \
+	--enable-libx264
 
 
 #	--enable-debug \
