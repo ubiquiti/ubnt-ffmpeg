48f038548af6a8ddafd9dd6f49cc554719d19e09
Gavriloaie Eugen-Andrei <andrei.gavriloaie@ui.com>
2020-11-18 00:58:03 +0200

    -- minor

--- debug.sh
+++ debug.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-export TEMP_PREFIX=/tmp/fftests/external_libs/ffmpeg
-export FFMPEG_EXTRA_CFLAGS='-mmacosx-version-min=10.11'
+DEST_FOLDER=${LOCAL_PREFIX}
+PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${LOCAL_PREFIX}/lib/pkgconfig
 
-git clean -dxff ./
-
-if [ ! -f ./.configured ]
-then
+if [ ! -f ./.configured ]; then
 	./configure \
-		--prefix=${TEMP_PREFIX} \
-		--extra-cflags="${FFMPEG_EXTRA_CFLAGS} -I${TEMP_PREFIX}/include -g" \
-		--extra-ldflags="-L${TEMP_PREFIX}/lib" \
-		--enable-debug \
-		--disable-optimizations \
-		`cat ./features.txt|grep -v "^#"|tr "\n" " "`
+		--prefix=${DEST_FOLDER} \
+		--extra-cflags="-fno-stack-check -I${LOCAL_PREFIX}/include -I${DEST_FOLDER}/include" \
+		--extra-ldflags="-L${LOCAL_PREFIX}/lib -L${DEST_FOLDER}/lib" \
+		--enable-gpl \
+		--enable-nonfree \
+		--enable-libx264 \
+		--enable-libfdk-aac \
+		--enable-libopus \
+		--disable-encoder=opus \
+		--disable-decoder=opus
 	>./.configured
 fi
 
-mv ${TEMP_PREFIX}/lib/libx264.a /tmp
-rm -rf ${TEMP_PREFIX}
-make -j32 install
-mv /tmp/libx264.a ${TEMP_PREFIX}/lib/
->${TEMP_PREFIX}/.extracted
+make -j32
+./ffmpeg -y -rtsp_transport tcp -i rtsp://10.0.20.2:9997/0418D6A0F52E_0 -acodec copy -vcodec copy -f flv /dev/null
